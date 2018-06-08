bd69be24feb3a7265600ea6ea280d2f72ef22e42
alishaevn <alieva.inc@gmail.com>
2018-06-08 06:54:13 -0700

    board and list components auto added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardsComponent } from './boards/boards.component';
+import { ListsComponent } from './lists/lists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardsComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule
