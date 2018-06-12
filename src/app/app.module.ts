b181ab47a36ee0a67660c97858493d315e7915fa
alishaevn <alieva.inc@gmail.com>
2018-06-11 19:44:26 -0700

    added FormsModule to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 // COMPONENTS
 import { AppComponent } from './app.component';
 import { BoardsComponent } from './boards/boards.component';
 import { ListsComponent } from './lists/lists.component';
+import { CardsComponent } from './cards/cards.component';
+import { CardFormComponent } from './card-form/card-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardsComponent,
-    ListsComponent
+    ListsComponent,
+    CardsComponent,
+    CardFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
